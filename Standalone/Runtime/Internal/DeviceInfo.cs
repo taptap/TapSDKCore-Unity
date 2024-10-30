d8c6926ec5dd89c3e35d2bc59c36de674151aff4
lc1006 <1176168734@qq.com>
2024-10-31 01:51:24 +0800

    feat:update upm

---
+++ Standalone/Runtime/Internal/DeviceInfo.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+namespace TapSDK.Core.Standalone.Internal {
+    public class DeviceInfo {
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        [DllImport("TapDBDeviceInfo", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr GetDeviceLanguage();
+#endif
+
+        public static string GetLanguage() {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            return Marshal.PtrToStringAnsi(GetDeviceLanguage());
+#else
+            return CultureInfo.CurrentUICulture.IetfLanguageTag;
+#endif
+        }
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetProcessTimes(IntPtr processHandle,
+            out long creationTime,
+            out long exitTime,
+            out long kernelTime,
+            out long userTime);
+
+        static DateTime GetProcessStartTime()
+        {
+            IntPtr processHandle = GetCurrentProcess();
+            long creationTime;
+            GetProcessTimes(processHandle,
+                out creationTime,
+                out _,
+                out _,
+                out _);
+
+            return DateTime.FromFileTime(creationTime);
+        }
+#endif
+
+        //安全组提供的设备识别 ID 算法，用于后续数据串联
+        public static string GetLaunchUniqueID() 
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+          // 获取当前进程对象
+          Process currentProcess = Process.GetCurrentProcess();
+          // 获取进程启动时间
+          DateTime startTime = GetProcessStartTime();
+          return toMd5(startTime.ToFileTime().ToString() + "-" + currentProcess.Id.ToString());
+#else
+          return "";
+#endif
+
+        }
+
+        public static void GetMacAddress(out string macAddressList, out string firstMacAddress)
+        {
+          List<string> mac_addrs = new List<string>();
+
+          try
+          {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in nics)
+            {
+              string physicalAddress = adapter.GetPhysicalAddress().ToString();
+              if (string.IsNullOrEmpty(physicalAddress))
+                continue;
+
+              physicalAddress = $"\"{physicalAddress}\"";
+              if (mac_addrs.IndexOf(physicalAddress) == -1)
+                mac_addrs.Add(physicalAddress);
+            }
+            // sort
+            mac_addrs.Sort();
+          }
+          catch (Exception e)
+          {
+            UnityEngine.Debug.Log("GetMacAddress Exception " + e.Message);
+          }
+          macAddressList = $"[{string.Join(",", mac_addrs)}]";
+          firstMacAddress = mac_addrs.Count > 0 ? mac_addrs[0].Replace("\"", "") : string.Empty;
+        }
+
+        private static string toMd5(string data)
+        {
+          byte[] buffer = System.Text.Encoding.Default.GetBytes(data);
+          try
+          {
+            System.Security.Cryptography.MD5CryptoServiceProvider chk = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] some = chk.ComputeHash(buffer);
+            string ret = "";
+            foreach(byte a in some)
+            {
+                if (a < 16)
+                    ret += "0" + a.ToString("X");
+                else
+                    ret += a.ToString("X");                    
+            }
+            return ret.ToLower();
+          }
+          catch
+          {
+              throw;
+            }
+        }
+
+        public static string RAM
+        {
+          get {
+            return (SystemInfo.systemMemorySize * 1024L * 1024L).ToString();
+          }
+        }
+        
+        // 获取系统的 ROM 存储容量，单位为 Byte
+        public static string ROM
+        {
+          get {
+            string path = Application.persistentDataPath;
+            if (string.IsNullOrEmpty(path))
+              return "0";
+
+            try
+            {
+              System.IO.DriveInfo drive = new System.IO.DriveInfo(path);
+              return drive.TotalSize.ToString();
+            }
+            catch (Exception e)
+            {
+              UnityEngine.Debug.Log("GetROMInByte Exception " + e.Message);
+              return "0";
+            }
+          }
+        }
+      
+        public static string Local
+        {
+          get {
+            return CultureInfo.CurrentCulture.Name.Replace('-', '_');
+          }
+        }
+    }
+}
---
+++ Standalone/Runtime/Internal/Http/TapHttpUtils.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using TapSDK.Core.Internal.Log;
+using TapSDK.Core.Standalone.Internal.Service;
+using UnityEngine;
+
+namespace TapSDK.Core.Standalone.Internal.Http
+{
+    public static class TapHttpTime
+    {
+        private static int timeOffset = 0;
+        private static void SetTimeOffset(int offset)
+        {
+            timeOffset = offset;
+        }
+
+        // 获取当前时间的秒级时间戳
+        public static int GetCurrentTime()
+        {
+            DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan timeSpan = DateTime.UtcNow - epochStart;
+            return (int)timeSpan.TotalSeconds + timeOffset;
+        }
+
+        public static void FixTime(int time)
+        {
+            if (time == 0)
+            {
+                return;
+            }
+            SetTimeOffset(time - (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds);
+        }
+    }
+
+    public static class TapHttpUtils
+    {
+
+        private static readonly TapLog tapLog = new TapLog("Http");
+
+        internal static string GenerateNonce()
+        {
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            char[] nonce = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                nonce[i] = chars[UnityEngine.Random.Range(0, chars.Length)];
+            }
+            return new string(nonce);
+        }
+
+        internal static string GenerateUserAgent()
+        {
+            return $"TapSDK-Unity/{TapTapSDK.Version}";
+        }
+
+        internal static string GenerateTime()
+        {
+            return TapHttpTime.GetCurrentTime().ToString();
+        }
+
+        internal static string GenerateLanguage()
+        {
+            return Tracker.getServerLanguage();
+        }
+
+        internal static string GenerateDeviceId()
+        {
+            return SystemInfo.deviceUniqueIdentifier;
+        }
+
+        internal static string GenerateAuthorization(string url, string method)
+        {
+            Type interfaceType = typeof(ITapLoginService);
+            Type[] initTaskTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(asssembly =>
+                {
+                    string fullName = asssembly.GetName().FullName;
+                    return fullName.StartsWith("TapSDK.Login.Standalone.Runtime");
+                })
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(clazz =>
+                {
+                    return interfaceType.IsAssignableFrom(clazz) && clazz.IsClass;
+                })
+                .ToArray();
+            if (initTaskTypes.Length != 1)
+            {
+                return null;
+            }
+            try
+            {
+                ITapLoginService tapLoginService = Activator.CreateInstance(initTaskTypes[0]) as ITapLoginService;
+                string authorization = tapLoginService.ObtainAuthorizationAsync(url, method);
+                return authorization;
+            }
+            catch (Exception e)
+            {
+                TapLog.Error("e = " + e);
+            }
+            return null;
+        }
+
+        public static void PrintRequest(HttpClient client, HttpRequestMessage request)
+        {
+            if (TapLogger.LogDelegate == null)
+            {
+                return;
+            }
+            if (client == null)
+            {
+                return;
+            }
+            if (request == null)
+            {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("=== HTTP Request Start ===");
+            sb.AppendLine($"URL: {request.RequestUri}");
+            sb.AppendLine($"Method: {request.Method}");
+            sb.AppendLine($"Headers: ");
+            foreach (var header in client.DefaultRequestHeaders)
+            {
+                sb.AppendLine($"\t{header.Key}: {string.Join(",", header.Value.ToArray())}");
+            }
+            foreach (var header in request.Headers)
+            {
+                sb.AppendLine($"\t{header.Key}: {string.Join(",", header.Value.ToArray())}");
+            }
+            if (request.Content != null)
+            {
+                foreach (var header in request.Content.Headers)
+                {
+                    sb.AppendLine($"\t{header.Key}: {string.Join(",", header.Value.ToArray())}");
+                }
+            }
+            string contentString = null;
+            try
+            {
+                contentString = request.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception)
+            {
+            }
+            if (!string.IsNullOrEmpty(contentString))
+            {
+                sb.AppendLine($"Content: \n{contentString}");
+            }
+            sb.AppendLine("=== HTTP Request End ===");
+            tapLog.Log($"HTTP Request [{request.RequestUri.PathAndQuery}]", sb.ToString());
+        }
+
+        public static void PrintResponse(HttpResponseMessage response)
+        {
+            if (TapLogger.LogDelegate == null)
+            {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("=== HTTP Response Start ===");
+            sb.AppendLine($"URL: {response.RequestMessage.RequestUri}");
+            sb.AppendLine($"Status Code: {response.StatusCode}");
+            string contentString = null;
+            try
+            {
+                contentString = response.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception)
+            {
+            }
+            if (!string.IsNullOrEmpty(contentString))
+            {
+                sb.AppendLine($"Content: {contentString}");
+            }
+            sb.AppendLine("=== HTTP Response End ===");
+            tapLog.Log($"HTTP Response [{response.RequestMessage.RequestUri.PathAndQuery}]", sb.ToString());
+        }
+
+    }
+}
